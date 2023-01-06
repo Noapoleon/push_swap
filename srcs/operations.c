8b2b8f9cd8f55b0582647b0a2cabc950a49ca06a
Noapoleon <noalegrand088@gmail.com>
2023-01-06 14:43:49 +0100

    idk what i changed who cares

--- srcs/operations.c
+++ srcs/operations.c
@@ -6,7 +6,7 @@
 /*   By: nlegrand <nlegrand@stud.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 12:50:43 by nlegrand          #+#    #+#             */
-/*   Updated: 2023/01/06 13:29:03 by nlegrand         ###   ########.fr       */
+/*   Updated: 2023/01/06 13:58:36 by nlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	push(t_stack *dst, t_stack *src)
 	t_stack_list	*pushing;
 
 	if (src->size == 0) // useless but keep for debugging with a print
-		return ((void)ft_printf("nothing to push\n"));
+		return ((void)ft_printf("nothing to push\n")); // REMOOOOOOOOOOOOOOOOOOVE PRINT LATER
 	pushing = src->top;
 	if (src->size-- == 1)
 		src->top = NULL;
@@ -46,7 +46,7 @@ void	swap(t_stack *s)
 	int	tmp;
 
 	if (s->size < 2) // put for debug only, but this might be useful
-		return ((void)ft_printf("nothing to swap\n"));
+		return ((void)ft_printf("nothing to swap\n")); // REMOOOOOOOOOOOOOOOOOOVE PRINT LATER
 	tmp = s->top->data;
 	s->top->data = s->top->next->data;
 	s->top->next->data = tmp;
@@ -55,13 +55,13 @@ void	swap(t_stack *s)
 void	rot(t_stack *s)
 {
 	if (s->size < 2) // put for debug only, but this might be useful
-		return ((void)ft_printf("nothing to rot\n"));
+		return ((void)ft_printf("nothing to rot\n")); // REMOOOOOOOOOOOOOOOOOOVE PRINT LATER
 	s->top = s->top->next;
 }
 
 void	rrot(t_stack *s)
 {
 	if (s->size < 2) // put for debug only, but this might be useful
-		return ((void)ft_printf("nothing to rrot\n"));
+		return ((void)ft_printf("nothing to rrot\n")); // REMOOOOOOOOOOOOOOOOOOVE PRINT LATER
 	s->top = s->top->prev;
 }
